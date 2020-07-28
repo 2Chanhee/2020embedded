67f304f5bcccb08d01ff0b93d0f0da6039aeadb5
2Chanhee <bawoo1029@naver.com>
2020-07-28 14:33:21 +0900

    modified:   rapi/rapi.py 	modified:   try/hsv1.py

--- rapi/rapi.py
+++ rapi/rapi.py
@@ -19,16 +19,21 @@ direction = False # False == Left
 
 # LineTracing
 def LineTracing(src):
-
+    # src = cv2.equalizeHist(src)
     degree = np.array([])
     # line   = np.array([])
     # Convert BGR to Gray for reduce time
     # src = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    s = cv2.inRange(s, 40, 255)
+    hsv = cv2.bitwise_and(hsv, hsv, mask = s)
+    src = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    src = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
 
     # Detect line using hough transformation
-    src = cv2.equalizeHist(src)
+    
     canny = cv2.Canny(src, 50, 200)
     line  = cv2.HoughLines(canny, 1, np.pi/180, 50)
 
@@ -42,29 +47,34 @@ def LineTracing(src):
     if np.any( np.abs(degree) > 1.25 ) :
         vertical = True
         comu.TX_data(comu.serial_port, ORD_STRAIGHT)
+        print("STR")
 
     elif np.any( degree < 0) :
         vertical = False
         comu.TX_data(comu.serial_port, ORD_TURNLEFT)
+        print("LFT")
 
     else :
         vertical = False
         comu.TX_data(comu.serial_port, ORD_TURNRIGHT)
+        print("RGT")
     
     if vertical == True & ( np.any( np.abs(degree) < 0.3 ) ):
         horiznal = True
+        print("CRS")
         if direction :
             comu.TX_data(comu.serial_port, ORD_TURNLEFT_90)
         else :
             comu.TX_data(comu.serial_port, ORD_TURNRIGHT_90)
+        horiznal = False
 
 # main
 #if __name__ == "__main__":
 
 cap = cv2.VideoCapture(0)
 
-cap.set(3, 640)
-cap.set(4, 480)
+cap.set(3, 1280)
+cap.set(4, 960)
 
 ret, frame = cap.read()
 
@@ -76,6 +86,7 @@ else :
 
 while(True):
     ret, frame = cap.read()
+    frame = cv2.resize(frame, dsize = (640, 480), interpolation = cv2.INTER_NEAREST)
     cv2.imshow('test', frame)
     a = cv2.waitKey(1)
     if ret :
--- try/hsv1.py
+++ try/hsv1.py
@@ -3,6 +3,15 @@
 import cv2
 import numpy as np
 
+def findLine(src):
+    #src = cv2.equalizeHist(src)
+    hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    s = cv2.inRange(s, 70, 255)
+    hsv = cv2.bitwise_and(hsv, hsv, mask = s)
+    src = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return src
+
 cap = cv2.VideoCapture(0)
 #ret, frame = cap.read()
 
@@ -10,13 +19,14 @@ cap = cv2.VideoCapture(0)
 #    print('Camera is not open!')
 #    exit()
 
-cap.set(3, 2560)
-cap.set(4, 1920)
+cap.set(3, 640)
+cap.set(4, 480)
 
 #print(frame.shape)
 while(True):
     ret, frame = cap.read()
-    frame = cv2.resize(frame, dsize = (320, 240), interpolation = cv2.INTER_NEAREST)    
+    #frame = cv2.resize(frame, dsize = (320, 240), interpolation = cv2.INTER_NEAREST)    
+    frame = findLine(frame)
     cv2.imshow("Debug", frame)
     a = cv2.waitKey(1)
     if a == ord('q'):
